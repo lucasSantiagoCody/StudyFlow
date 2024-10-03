172bc1a738a0f5938cfe3b0acc86a22b530aa86f
lucascody <lucassantiagocody@gmail.com>
2024-10-03 20:58:35 +0100

    Remove STATIC_ROOT constant from core's settings.py file

--- core/settings.py
+++ core/settings.py
@@ -105,7 +105,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
